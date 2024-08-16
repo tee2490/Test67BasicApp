e4d6740f1635a3e94beb53becb8d609f5b27bf70
Teeradet <tee2490@hotmail.com>
2024-08-16 09:41:53 +0700

    Text file : สร้างและอ่าน

---
+++ ConsoleApp4/Program.cs
@@ -0,0 +1,14 @@
+﻿// Write file using StreamWriter
+
+var fullPath = "E:\\cs67\\Basic\\BasicApp\\ConsoleApp4\\Test.txt";
+using (StreamWriter writer = new StreamWriter(fullPath))
+{
+    writer.WriteLine("Monica Rathbun");
+    writer.WriteLine("Vidya Agarwal");
+    writer.WriteLine("Mahesh Chand");
+    writer.WriteLine("Vijay Anand");
+    writer.WriteLine("Jignesh Trivedi");
+}
+// Read a file
+string readText = File.ReadAllText(fullPath);
+Console.WriteLine(readText);
