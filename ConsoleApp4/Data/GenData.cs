5e7d8780c7b6203b01707afee27c988fbde68f10
Teeradet <tee2490@hotmail.com>
2024-08-16 10:10:17 +0700

    TextFile :  Class and Create file

---
+++ ConsoleApp4/Data/GenData.cs
@@ -0,0 +1,51 @@
+﻿using ConsoleApp4.Models;
+
+namespace ConsoleApp4.Data
+{
+    public class GenData
+    {
+        List<Product> ProductList { get; set; }
+
+        public GenData() 
+        {
+            ProductList = new List<Product>();
+            Create();
+            WriteToFile();
+        }
+
+        public void Create(int number = 10)
+        {
+            Random random = new Random();
+
+            for (int i = 0; i < number; i++) 
+            {
+                Product product = new Product()
+                {
+                    Id =i,
+                     Name = "ROV"+i,
+                     Price = random.Next(10,1000)+random.NextDouble(),
+                     Amount = random.Next(10,20)
+                };
+
+                ProductList.Add(product);
+            }
+        }
+
+        public void WriteToFile()
+        {
+            var fullPath = "E:\\cs67\\Basic\\BasicApp\\ConsoleApp4\\Test.txt";
+            using (StreamWriter writer = new StreamWriter(fullPath))
+            {
+                foreach (var item in ProductList)
+                {
+                    writer.WriteLine($"{item.Id} {item.Name} {item.Price.ToString("##.##")} {item.Amount}");
+                }
+                writer.Close();
+            }
+        
+        }
+
+
+
+    }
+}
--- ConsoleApp4/Program.cs
+++ ConsoleApp4/Program.cs
@@ -1,14 +1,4 @@
-﻿// Write file using StreamWriter
+﻿
+using ConsoleApp4.Data;
 
-var fullPath = "E:\\cs67\\Basic\\BasicApp\\ConsoleApp4\\Test.txt";
-using (StreamWriter writer = new StreamWriter(fullPath))
-{
-    writer.WriteLine("Monica Rathbun");
-    writer.WriteLine("Vidya Agarwal");
-    writer.WriteLine("Mahesh Chand");
-    writer.WriteLine("Vijay Anand");
-    writer.WriteLine("Jignesh Trivedi");
-}
-// Read a file
-string readText = File.ReadAllText(fullPath);
-Console.WriteLine(readText);
+var ps = new GenData();
